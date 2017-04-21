f39ce6a7272bbe0f7cb270854e8d8f5f4c4004e5
adolphlwq <kenan3015@gmail.com>
2017-04-21 18:21:24 +0800

    tmp

---
+++ leetcode/dp/lcs.py
@@ -0,0 +1,11 @@
+class LCS(object):
+    """
+    LCS: Longest Common Substring
+    e.g: 'adscrfs'--> '
+    """
+    def lcs(self, s1, s2):
+        """
+        :type s: string
+        :rtype: substring
+        """
+
