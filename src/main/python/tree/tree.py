5e1ec8ee81cce6843f4c30de5b5158443c3627ac
adolphlwq <kenan3015@gmail.com>
2017-05-04 00:04:23 +0800

    binary tree node impleted

---
+++ src/main/python/tree/tree.py
@@ -0,0 +1,44 @@
+class NodeBinaryTree:
+    def __init__(self, root):
+        self.key = root
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left(self, key):
+        node = NodeBinaryTree(key)
+        if self.left_child:
+            node.left_child = self.left_child
+        self.left_child = node
+
+    def insert_right(self, key):
+        node = NodeBinaryTree(key)
+        if self.right_child:
+            node.right_child = self.right_child
+        self.right_child = node
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root(self, key):
+        self.key = key
+
+    def get_root(self):
+        return self.key
+
+def test_node_bt():
+    t = NodeBinaryTree('root')
+    assert t.get_root() == 'root'
+    t.insert_left(1)
+    assert t.get_left_child().get_root() == 1
+    t.insert_right(2)
+    assert t.get_right_child().get_root() == 2
+    t.insert_right(3)
+    assert t.get_right_child().get_root() == 3
+
+
+class ListBinaryTree:
+    def __init__(self):
+        print("to be impleted")
