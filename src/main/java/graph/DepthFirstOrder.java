93f542c5d2d15c7ded5133c772a2d7fdc307399e
adolphlwq <kenan3015@gmail.com>
2018-03-04 23:29:06 +0800

    toplogic sort

---
+++ src/main/java/graph/DepthFirstOrder.java
@@ -0,0 +1,95 @@
+import edu.princeton.cs.algs4.*;
+
+public class DepthFirstOrder{
+    private boolean[] marked;
+    private int[] pre;
+    private int[] post;
+    private Queue<Integer> preorder;
+    private Queue<Integer> postorder;
+    private int preCounter;
+    private int postCounter;
+
+    public DepthFirstOrder(Digraph G) {
+        marked = new boolean[G.V()];
+        pre = new int[G.V()];
+        post = new int[G.V()];
+        preorder = new Queue<Integer>();
+        postorder = new Queue<Integer>();
+
+        for (int v=0; v<G.V(); v++) {
+            if (!marked[v]) {
+                dfs(G, v);
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        pre[v] = preCounter++;
+        preorder.enqueue(v);
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+        postorder.enqueue(v);
+        post[v] = postCounter++;
+    }
+
+    public int pre(int v) {
+        return pre[v];
+    }
+
+    public int post(int v) {
+        return post[v];
+    }
+
+    public Iterable<Integer> post() {
+        return postorder;
+    }
+
+    public Iterable<Integer> pre() {
+        return preorder;
+    }
+
+    public Iterable<Integer> reversePost() {
+        Stack<Integer> reverse = new Stack<Integer>();
+        for (int v : postorder) {
+            reverse.push(v);
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        Digraph G = new Digraph(in);
+
+        DepthFirstOrder dfs = new DepthFirstOrder(G);
+        StdOut.println("   v  pre post");
+        StdOut.println("--------------");
+        for (int v = 0; v < G.V(); v++) {
+            StdOut.printf("%4d %4d %4d\n", v, dfs.pre(v), dfs.post(v));
+        }
+
+        StdOut.print("Preorder:  ");
+        for (int v : dfs.pre()) {
+            StdOut.print(v + " ");
+        }
+        StdOut.println();
+
+        StdOut.print("Postorder: ");
+        for (int v : dfs.post()) {
+            StdOut.print(v + " ");
+        }
+        StdOut.println();
+
+        StdOut.print("Reverse postorder: ");
+        for (int v : dfs.reversePost()) {
+            StdOut.print(v + " ");
+        }
+        StdOut.println();
+
+
+    }
+    
+}
