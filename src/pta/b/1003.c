40a8f69df338b56157673e84855b3b57c69d5fc5
adolphlwq <kenan3015@gmail.com>
2019-03-11 14:50:38 +0800

    update 1003

--- src/pta/b/1003.c
+++ src/pta/b/1003.c
@@ -1,26 +1,50 @@
 // https://pintia.cn/problem-sets/994805260223102976/problems/994805323154440192
 #include <stdio.h>
+#include <string.h>
 
-#define MAXN 100
-
-int check(char *s);
+#define MAXN 101
 
 int main() {
-    printf("Not implement\n");
-    return 0;
-
     int i, N;
     char s[MAXN];
+    char *sp;
     scanf("%d", &N);
 
+    int a, b, c;
     for(i=0; i<N; i++) {
         scanf("%s", s);
-        if(check(s))    printf("YES\n");
-        else            printf("NO\n");
-    }
-    return 0;
-}
+        if(strcmp(s, "PAT") == 0) {
+            printf("YES\n");
+            continue;
+        }
+        
+        a=b=c=0;
+        sp = s;
+        while(*sp == 'A') {
+            a++;
+            sp++;
+        }
+        if(*sp == 'P') {
+            sp++;
+            while(*sp == 'A') {
+                b++;
+                sp++;
+            }
+            if(*sp == 'T') {
+                sp++;
+                while(*sp == 'A') {
+                    c++;
+                    sp++;
+                }
+                if(!(*sp) && b>0 && c==a*b) {
+                    printf("YES\n");
+                    continue;
+                }
+            }
+        }
 
-int check(char *s) {
-    return 1;
+        printf("NO\n");
+    }
+
+    return 0;
 }
